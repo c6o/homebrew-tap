79994429712bfe335055e3062edf472c5e3a35be
Georg Friedrich <georg@codezero.io>
2024-10-10 05:12:07 +1100

    Fix Intel URL in codezero-app@beta.rb

--- Casks/codezero-app@beta.rb
+++ Casks/codezero-app@beta.rb
@@ -5,7 +5,7 @@ cask "codezero-app@beta" do
   sha256 arm:   "82270787e844b2c4455987a72f59515db5d0208a719f7fb0d3b82e2524daa3ed",
          intel: "1a04b882549be2b223da55adbcf2bc2893b10388c72cb3db82dc2fe2ccd8400f"
 
-  url "https://releases.codezero.io/app/darwin/arm64/Codezero-darwin-#{arch}-#{version}.zip"
+  url "https://releases.codezero.io/app/darwin/#{arch}/Codezero-darwin-#{arch}-#{version}.zip"
   name "Codezero App"
   desc "Collaborative Local Microservices Development"
   homepage "https://codezero.io"
