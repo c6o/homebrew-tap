44da0ea1f0c93f43365b26dc7f803fd5916ec18c
Georg Friedrich <georg@codezero.io>
2024-10-16 15:10:00 +1100

    Brew cask update for codezero-app version 2.7.0-beta6

--- Casks/codezero-app@beta.rb
+++ Casks/codezero-app@beta.rb
@@ -1,11 +1,11 @@
 cask "codezero-app@beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.7.0-beta4"
-  sha256 arm:   "82270787e844b2c4455987a72f59515db5d0208a719f7fb0d3b82e2524daa3ed",
-         intel: "1a04b882549be2b223da55adbcf2bc2893b10388c72cb3db82dc2fe2ccd8400f"
+  version "2.7.0-beta6"
+  sha256 arm:   "3491e5857c71864eb6f763c51f2b74f9981fe6d601a0a0b6bc57b35a4e01d21f",
+         intel: "cba39f2634eb6d0e04474673516d8f230ab2f5af245939c079573ab4ca683f31"
 
-  url "https://releases.codezero.io/app/darwin/#{arch}/Codezero-darwin-#{arch}-#{version}.zip"
+  url "https://releases.codezero.io/app/darwin/#{arch}/Codezero-darwin-#{arch}-#{version}-mac.zip"
   name "Codezero App"
   desc "Collaborative Local Microservices Development"
   homepage "https://codezero.io"
@@ -14,6 +14,7 @@ cask "codezero-app@beta" do
   conflicts_with cask: "codezero-app"
 
   app "Codezero.app"
+  binary "#{appdir}/Codezero.app/Contents/Resources/bin/czctl"
 
   uninstall launchctl: "io.codezero.czctl",
             quit:      "io.codezero.codezero" 
